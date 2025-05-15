642955231e6777c205b2242659f5e09d910a955b
Amir <mosallanejad.amir@gmail.com>
2025-05-15 19:33:58 +0330

    Add project files.

---
+++ FactoryMethodPatternConsole/NotificationFactory.cs
@@ -0,0 +1,13 @@
+﻿public class NotificationFactory
+{
+    public INotificationService Create(string channel)
+    {
+        return channel.ToLower() switch
+        {
+            "email" => new EmailNotification(),
+            "sms" => new SmsNotification(),
+            "push" => new PushNotification(),
+            _ => throw new ArgumentException("Unsupported notification channel", nameof(channel))
+        };
+    }
+}
---
+++ FactoryMethodPatternConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Factory Method Pattern");
+
+var factory = new NotificationFactory();
+
+INotificationService notification = factory.Create("email");
+notification.Send("Welcome to our service!");
+
+notification = factory.Create("sms");
+notification.Send("Your OTP is 123456");
+
+notification = factory.Create("push");
+notification.Send("You have a new message!");
+
+Console.ReadKey();
